f957e5cc6babe8a0fd9b27729e4afa07779ac6d8
yys-0505 <yinyishuai@126.com>
2019-03-25 17:54:42 +0800

    tiny change

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './js/a.vue'
-require("./css/index.scss");
+// import css from './css/index.css';
+// import less from './css/index.less';
+import scss from './css/index.scss';
 new Vue({
     el: "#root",
     render:h=>h(App)
